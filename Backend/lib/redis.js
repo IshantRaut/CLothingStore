faa46ee71129fcf59dc833bcec15f23aa2afb544
IshantRaut <ishjee2018@gmail.com>
2025-09-20 15:39:01 +0530

    Working on Backend Cart Part 2

--- Backend/lib/redis.js
+++ Backend/lib/redis.js
@@ -1,25 +1,5 @@
-import { createClient } from "redis"
-import dotenv from 'dotenv'
-dotenv.config({quiet:true});
-export const redis = createClient({
-  url: process.env.UPSTASH_REDIS_URL
-});
+import Redis from "ioredis";
+import dotenv from "dotenv";
+dotenv.config();
 
-async function connectRedis() {
-    try {
-        await redis.connect();
-        console.log("Redis Connected");
-        return true;
-    } catch (error) {
-        console.error("Redis Connection Error:", error);
-        return false;
-    }
-}
-
-const isConnected = await connectRedis();
-
-
-
-if (!isConnected) {
-  console.log("Redis Not Connected");
-}
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
--- Backend/routes/cartRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { addToCart } from '../controllers/cartControllers'
-
-const cartRouter = express.Router();
-
-cartRouter.post('/', addToCart);
-
-export default cartRouter;
