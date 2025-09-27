7b5a99f78f6f11aa9df6e4a4e317f86d834ace6c
IshantRaut <ishjee2018@gmail.com>
2025-09-27 21:31:29 +0530

    Deployment Bug Fixes

--- Backend/server.js
+++ Backend/server.js
@@ -36,9 +36,11 @@ app.use("/api/cart", cartRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
+// Catch-all route for frontend
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 }
 
 app.listen(PORT, () => {
