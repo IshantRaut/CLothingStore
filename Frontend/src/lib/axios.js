a3883fd3db87f4316d1efc4282b9e584c4c6f8e6
IshantRaut <ishjee2018@gmail.com>
2025-09-27 21:50:23 +0530

    Deploying backend url with frontend

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// Replace this with your actual backend Render URL
+const BACKEND_URL = "https://clothingstore-w3w0.onrender.com/api";
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.DEV ? "http://localhost:4000/api" : "/api", // 👈 fixed
+  baseURL: import.meta.env.DEV ? "http://localhost:4000/api" : BACKEND_URL,
   withCredentials: true, // send cookies
 });
 
