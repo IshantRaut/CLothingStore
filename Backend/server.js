78542513fe460b065a01a1237208d52c2c095e32
IshantRaut <ishjee2018@gmail.com>
2025-09-27 22:06:05 +0530

    Deployment Bug fix

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import path from "path";
-import cors from "cors";  
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -16,11 +15,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-const __dirname = path.resolve();
-
 app.use(cors({
-  origin: process.env.FRONTEND_URL, 
-  credentials: true,              
+  origin: [
+    process.env.FRONTEND_URL, 
+    "http://localhost:5173"
+  ],
+  credentials: true,
 }));
 
 app.use(express.json({ limit: "10mb" }));
@@ -31,17 +31,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
-app.use("/api/cart", cartRoutes);  
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-// Catch-all route for frontend
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
-
-}
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
