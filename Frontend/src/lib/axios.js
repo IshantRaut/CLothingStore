1d8c57c0c38d3f1fdb8abdcf23557ce0ae195d2b
IshantRaut <ishjee2018@gmail.com>
2025-09-28 12:56:01 +0530

    Frontend URL in Backend

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.DEV ? "https://clothingstore-1jmp.onrender.com/api" : "/api", // 👈 fixed
-  withCredentials: true, // send cookies
+  baseURL: import.meta.env.DEV
+    ? "http://localhost:4000/api" 
+    : "https://clothingstore-1jmp.onrender.com/api", // point to backend service
+  withCredentials: true,
 });
+
 
 export default axiosInstance;
